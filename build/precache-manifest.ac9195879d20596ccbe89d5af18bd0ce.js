20a39d802ffd825dabfbfe8d27ff47ca24092447
ArtemRakhimov <artemiirakhimov@yandex.ru>
2020-04-30 17:45:25 +0300

    build

---
+++ build/precache-manifest.ac9195879d20596ccbe89d5af18bd0ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf9f9a46a27e8190caf5c19f1b2e5ca",
+    "url": "/middle.react.praktikum.yandex/index.html"
+  },
+  {
+    "revision": "7e0862e5d538f7f1ea97",
+    "url": "/middle.react.praktikum.yandex/static/css/main.e647d99e.chunk.css"
+  },
+  {
+    "revision": "c366e19e91ea06d59ec7",
+    "url": "/middle.react.praktikum.yandex/static/js/2.86702f71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/middle.react.praktikum.yandex/static/js/2.86702f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e0862e5d538f7f1ea97",
+    "url": "/middle.react.praktikum.yandex/static/js/main.98c5aeb7.chunk.js"
+  },
+  {
+    "revision": "c7ce702d4e73cec005f5",
+    "url": "/middle.react.praktikum.yandex/static/js/runtime-main.5876ed40.js"
+  }
+]);
