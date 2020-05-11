8cfdc05fd452b57fad3204bb630064339ad7e9fe
ArtemRakhimov <artemiirakhimov@yandex.ru>
2020-05-11 22:12:34 +0300

    add routing

---
+++ src/data/chatList.ts
@@ -0,0 +1,100 @@
+import {ChatProps} from '../types';
+
+export const chatList: ChatProps[] = [
+  {
+    urlImage: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    name: 'Архыз',
+    id: '0001',
+    user: 'arakhimov',
+    date: 0,
+    lastMessageText: ''    
+  },
+  {
+    urlImage: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    name: 'Челябинская область',
+    id: '0002',
+    user: 'arakhimov',
+    date: 0,
+    lastMessageText: ''
+  },
+  {
+    urlImage: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    name: 'Иваново',
+    id: '0003',
+    user: 'arakhimov',
+    date: 0,
+    lastMessageText: ''
+  },
+  {
+    urlImage: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+    name: 'Камчатка',
+    id: '0004',
+    user: 'arakhimov',
+    date: 0,
+    lastMessageText: ''
+  },
+  {
+    urlImage: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+    name: 'Холмогорский район',
+    id: '0005',
+    user: 'arakhimov',
+    date: 0,
+    lastMessageText: ''
+  },
+  {
+    urlImage: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+    name: 'Байкал',
+    id: '0006',
+    user: 'arakhimov',
+    date: 0,
+    lastMessageText: ''
+  },
+  {
+    urlImage: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/khrebet-nurgush.jpg',
+    name: 'Нургуш',
+    id: '0007',
+    user: 'arakhimov',
+    date: 0,
+    lastMessageText: ''
+  },
+  {
+    urlImage: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/tulinovka.jpg',
+    name: 'Тулиновка',
+    id: '0008',
+    user: 'arakhimov',
+    date: 0,
+    lastMessageText: ''
+  },
+  {
+    urlImage: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/zheltukhin-island.jpg',
+    name: 'Остров Желтухина',
+    id: '0009',
+    user: 'arakhimov',
+    date: 0,
+    lastMessageText: ''
+  },
+  {
+    urlImage: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/vladivostok.jpg',
+    name: 'Владивосток',
+    id: '0010',
+    user: 'arakhimov',
+    date: 0,
+    lastMessageText: ''
+  },
+  {
+    urlImage: 'https://img.freepik.com/free-photo/_198169-183.jpg?size=338&ext=jpg',
+    name: 'One message',
+    id: '0011',
+    user: 'arakhimov',
+    date: 0,
+    lastMessageText: ''
+  },
+  {
+    urlImage: 'https://img.freepik.com/free-photo/_24972-737.jpg?size=626&ext=jpg',
+    name: 'Empty',
+    id: '0012',
+    user: 'arakhimov',
+    date: 0,
+    lastMessageText: ''
+  }
+];
---
+++ src/data/messageList.ts
@@ -0,0 +1,286 @@
+import {messageListProps} from '../types';
+
+// начальная и конечная даты сообщений
+const today: Date = new Date();
+const yearAgo: Date = new Date(2019, 0, 1);
+
+export const messageList: messageListProps = {
+  '0001': [
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    }
+  ],
+  '0002': [
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    }
+  ],
+  '0003': [
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    }
+  ],
+  '0004': [
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    }
+  ],
+  '0005': [
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    }
+  ],
+  '0006': [
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    }
+  ],
+  '0007': [
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    }
+  ],
+  '0008': [
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    }
+  ],
+  '0009': [
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    }
+  ],
+  '0010': [
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    },
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    }
+  ],
+  '0011': [
+    {
+      date: today.getTime() + Math.floor(Math.random() * (yearAgo.getTime() - today.getTime())),
+      author: 'arakhimov',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    }
+  ],
+  '0012': []
+};
