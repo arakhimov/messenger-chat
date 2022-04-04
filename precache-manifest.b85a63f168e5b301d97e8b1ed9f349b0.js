54378a17cae0edaf3863b81078a2a4c3fa5f3653
arahimov <artemiirakhimov@yandex.ru>
2022-04-04 20:38:10 +0300

    build project

---
+++ precache-manifest.b85a63f168e5b301d97e8b1ed9f349b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d17a101dd4315c76657203b282ee1a88",
+    "url": "/messenger-chat/index.html"
+  },
+  {
+    "revision": "fb120a9ce0084ebe287c",
+    "url": "/messenger-chat/static/css/main.0f318020.chunk.css"
+  },
+  {
+    "revision": "d21b28fbf2b33ae15735",
+    "url": "/messenger-chat/static/js/2.6f38cd85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/messenger-chat/static/js/2.6f38cd85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb120a9ce0084ebe287c",
+    "url": "/messenger-chat/static/js/main.78895609.chunk.js"
+  },
+  {
+    "revision": "09181ac998817f6009b5",
+    "url": "/messenger-chat/static/js/runtime-main.7dfd77b9.js"
+  }
+]);
